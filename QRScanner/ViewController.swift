888f565e760a8659257b58e7885b3ca5243dad76
Corentin <corentin.medina@epsi.fr>
2020-01-06 09:09:54 +0100

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Corentin Medina on 06/01/2020.
+//  Copyright © 2020 Corentin Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
